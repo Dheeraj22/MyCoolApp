29ff178b037dfc2c05f9bca5fe3072535db57541
Dheeraj Kamath <dheerajkamath22@gmail.com>
2020-04-22 21:07:28 +0530

    Initial commit

---
+++ RTE/_mtb-example-psoc6-hello-world/Pre_Include_Global.h
@@ -0,0 +1,27 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'mtb-example-psoc6-hello-world' 
+ * Target:  'mtb-example-psoc6-hello-world' 
+ */
+
+#ifndef PRE_INCLUDE_GLOBAL_H
+#define PRE_INCLUDE_GLOBAL_H
+
+/*  Cypress::TARGET_CY8CKIT-062-BLE:ModusToolbox */
+#define CY_USING_HAL
+#define CY_APPNAME_mtb_example_psoc6_hello_world
+#define CY_TARGET_DEVICE CY8C6347BZI_BLD53
+#define TARGET_CY8CKIT_062_BLE
+#define CY_TARGET_BOARD CY8CKIT_062_BLE
+#define COMPONENT_BSP_DESIGN_MODUS
+#define COMPONENT_CM0P_SLEEP
+#define COMPONENT_CM4
+#define COMPONENT_PSOC6HAL
+#define COMPONENT_SOFTFP
+#define DEBUG
+
+
+#endif /* PRE_INCLUDE_GLOBAL_H */
---
+++ RTE/_mtb-example-psoc6-hello-world/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'mtb-example-psoc6-hello-world' 
+ * Target:  'mtb-example-psoc6-hello-world' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
